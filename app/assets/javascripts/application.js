bff6353fdef01b9b0347900f97cd47b292c20ba6
Matt Chang <matthewchang3150@hotmail.com>
2016-08-25 23:39:55 -0700

    Completed working user authentication and added animations to homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,50 +32,62 @@
 
 // make navbar appear/disappear, not yet working
 
-// var didScroll;
-// var lastScrollTop = 0;
-// var delta = 5;
-// var navbarHeight = $('nav').outerHeight();
-//
-// $(window).scroll(function(event){
-//     didScroll = true;
-// });
-//
-// setInterval(function() {
-//     if (didScroll) {
-//         hasScrolled();
-//         didScroll = false;
-//     }
-// }, 250);
-//
-// function hasScrolled() {
-//     var st = $(this).scrollTop();
-//
-//     // Make sure they scroll more than delta
-//     if(Math.abs(lastScrollTop - st) <= delta)
-//         return;
-//
-//     // If they scrolled down and are past the navbar, add class .nav-up.
-//     // This is necessary so you never see what is "behind" the navbar.
-//     if (st > lastScrollTop && st > navbarHeight){
-//         // Scroll Down
-//         $('nav').removeClass('move').addClass('move');
-//     } else {
-//         // Scroll Up
-//         if(st + $(window).height() < $(document).height()) {
-//             $('nav').removeClass('move').addClass('move');
-//         }
-//     }
-//
-//     lastScrollTop = st;
-// }
+var didScroll;
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+
+  // my Code
+  if ($(this).scrollTop() > 200) {
+    $('.test-header').addClass('nav-down');
+  } else {
+    $('.test-header').removeClass('nav-down').addClass('nav-up');
+  }
+
+  if ($(this).scrollTop() > 500) {
+    $('.flip-image1').fadeIn();
+    $('.flip-image1').addClass('flipper');
+    $('.flip-image2').fadeIn();
+    $('.flip-image2').addClass('flipper');
+  } else {
+    $('.flip-image1').fadeOut();
+    $('.flip-image1').remove('flipper');
+    $('.flip-image2').fadeOut();
+    $('.flip-image2').remove('flipper');
+  }
+
+  if ($(this).scrollTop() > 800) {
+    $('.home-footer').fadeIn();
+    $('.home-footer').addClass('flipper');
+  } else {
+    $('.home-footer').fadeOut();
+    $('.home-footer').remove('flipper');
+  }
+
+}
 
 // control carousel speed
 $(document).ready(function(){
-     $("#myCarousel").carousel({
-         interval : 5000,
-         pause: false
-     });
+
+   $("#myCarousel").carousel({
+       interval : 5000,
+       pause: false
+   });
+
+   $('.flip-image1').hide();
+   $('.flip-image2').hide();
+   $('.home-footer').hide();
+
 });
 
 // Allow droppable elements
