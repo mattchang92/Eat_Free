04f25a70b41f5be455444639d3e47d68dc0394a2
Matt Chang <matthewchang3150@hotmail.com>
2016-08-30 23:10:54 -0700

    updated sliders with dynamic coloring

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'font-awesome-rails'
+gem 'bootstrap-glyphicons'
+gem 'font-awesome-sass'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,10 +22,13 @@
 // control carousel speed
 $(document).ready(function(){
 
-  $('#age-input').val($('#ageSlider').val());
-  $('#height-input').val($('#heightSlider').val());
-  $('#weight-input').val($('#weightSlider').val());
-  $('#weight-loss-input').val($('#weightLossSlider').val());
+
+
+  $('#age-input').val("20");
+  $('#height-input').val("100");
+  $('#weight-input').val("30");
+  $('#weight-loss-input').val("0.5");
+
 
    $("#myCarousel").carousel({
        interval : 5000,
@@ -80,15 +83,110 @@ $(document).ready(function(){
       $('#photo-container').scrollLeft($('#photo-container').scrollLeft()+1);
     },15);
 
+    var checkAgeSliderLength = function(){
+      var maxWidth = parseInt($('input.slide').css('width'));
+      var width = maxWidth * ((parseFloat($('#ageSlider').val())-20) /
+      80.3);
+      return width + 2 + 'px';
+    }
+
+    var checkAgeThumbSliderLength = function(){
+      var maxWidth = parseInt($('input.slide').css('width'));
+      var width = maxWidth * ((parseFloat($('#ageSlider').val())-20) /
+      80.3);
+      return width + 15 + 'px';
+    }
+
+    var checkWeightSliderLength = function(){
+      var maxWidth = parseInt($('input.slide').css('width'));
+      var width = maxWidth * ((parseFloat($('#weightSlider').val())-30) /
+      171);
+      return width + 2 + 'px';
+    }
+
+    var checkWeightThumbSliderLength = function(){
+      var maxWidth = parseInt($('input.slide').css('width'));
+      var width = maxWidth * ((parseFloat($('#weightSlider').val())-30) /
+      171);
+      return width + 15 + 'px';
+    }
+
+    var checkHeightSliderLength = function(){
+      var maxWidth = parseInt($('input.slide').css('width'));
+      var width = maxWidth * ((parseFloat($('#heightSlider').val())-100) /
+      151);
+      return width + 2 + 'px';
+    }
+
+    var checkHeightThumbSliderLength = function(){
+      var maxWidth = parseInt($('input.slide').css('width'));
+      var width = maxWidth * ((parseFloat($('#heightSlider').val())-100) /
+      151);
+      return width + 15 + 'px';
+    }
+
+    var checkLossSliderLength = function(){
+      var maxWidth = parseInt($('input.slide').css('width'));
+      var width = maxWidth * ((parseFloat($('#weightLossSlider').val())-0.5) /
+      2.5);
+      return width + 2 + 'px';
+    }
+
+    var checkLossThumbSliderLength = function(){
+      var maxWidth = parseInt($('input.slide').css('width'));
+      var width = maxWidth * ((parseFloat($('#weightLossSlider').val())-0.5) /
+      2.5);
+      return width + 15 + 'px';
+    }
+
+
+    $('#ageSlider').on('input change mousemove',function(){
+      var $element = $('#ageSlider');
+      $('#age-colored-slide').css('background-color', "rgb(" + getColor($element)*0.75 + "," + getColor($element)*0.75 + ",255)");
+      $('#age-slider-thumb').css('left',checkAgeThumbSliderLength());
+      $('#age-colored-slide').css('width',checkAgeSliderLength());
+    });
+
+    $('#weightSlider').on("input mousemove",function(){
+      var $element = $('#weightSlider');
+      $('#weight-colored-slide').css('background-color', "rgb(" + getColor($element)*0.75 + "," + getColor($element)*0.75 + ",255)");
+      $('#weight-colored-slide').css('width',checkWeightSliderLength());
+      $('#weight-slider-thumb').css('left',checkWeightThumbSliderLength());
+    });
+
+
+    $('#heightSlider').on("input mousemove",function(){
+      var $element = $('#heightSlider');
+      $('#height-colored-slide').css('background-color', "rgb(" + getColor($element)*0.75 + "," + getColor($element)*0.75 + ",255)");
+      $('#height-colored-slide').css('width',checkHeightSliderLength());
+      $('#height-slider-thumb').css('left',checkHeightThumbSliderLength());
+    });
+
+    $('#weightLossSlider').on("input mousemove",function(){
+      var $element = $('#weightLossSlider');
+      $('#loss-colored-slide').css('background-color', "rgb(" + getColor($element)*0.75 + "," + getColor($element)*0.75 + ",255)");
+      $('#loss-colored-slide').css('width',checkLossSliderLength());
+      $('#loss-slider-thumb').css('left',checkLossThumbSliderLength());
+    });
+
+
+    var getColor = function(element){
+      var max = parseFloat(element.attr('max')-element.attr('min'));
+      var current = parseFloat(element.val()-element.attr('min'));
+      return parseInt(255 - (160 * (current/max)));
+    };
+
 
     // Updates text fields with slider value
     $('.slide').mouseenter(function(){
       var checkSliderInput = setInterval(function(){
-        $('#age-input').val($('#ageSlider').val());
+        $('#age-input').val(parseInt($('#ageSlider').val()));
         $('#height-input').val($('#heightSlider').val());
         $('#weight-input').val($('#weightSlider').val());
         $('#weight-loss-input').val($('#weightLossSlider').val());
-      },50);
+
+
+      },20);
 
       $('.slide').mouseleave(function(){
         clearInterval(checkSliderInput)
@@ -112,25 +210,25 @@ $(document).ready(function(){
     // Button selection functionality for activity level
     $('#sedentary').click(function(){
       $('.activity').removeClass('option-selected');
-      $('#activity-level-input').val(1.2);
+      $('#activity-input').val(1.2);
       $(this).addClass('option-selected');
     })
 
     $('#lightly-active').click(function(){
       $('.activity').removeClass('option-selected');
-      $('#activity-level-input').val(1.433);
+      $('#activity-input').val(1.433);
       $(this).addClass('option-selected');
     })
 
     $('#active').click(function(){
       $('.activity').removeClass('option-selected');
-      $('#activity-level-input').val(1.666);
+      $('#activity-input').val(1.666);
       $(this).addClass('option-selected');
     })
 
     $('#very-active').click(function(){
       $('.activity').removeClass('option-selected');
-      $('#activity-level-input').val(1.9);
+      $('#activity-input').val(1.9);
       $(this).addClass('option-selected');
     })
 
@@ -154,6 +252,42 @@ $(document).ready(function(){
     //     clearInterval(checkWeight);
     //   }
     // }, 300)
+
+    var calculateCalories = function(){
+      var $age = parseInt($('#age-input').val());
+      var $weight = parseFloat($('#weight-input').val());
+      var $height = parseFloat($('#height-input').val());
+      var $activity = parseFloat($('#activity-input').val());
+
+      if ($('#sex-input').val() === 'male') {
+        return parseInt((66 + (13.7 * $weight) + (5 * $height) - (6.8 * $age)) * $activity);
+      } else if ($('#sex-input').val() === "female"){
+        return parseInt((655 + (9.6 * $weight) + (1.8 * $height) - (4.7 * $age)) * $activity);
+      }
+
+    }
+
+    var calculateDeficit = function(){
+      var $desired = parseFloat($('#weight-loss-input').val());
+      return ($desired * 500);
+    }
+
+    $('#stats-submit-btn').hover(function(){
+      // var a = calculateCalories;
+      // var b =  calculateDeficit;
+      // var result = a - b;
+      $('#calorie-result').html(calculateCalories() - calculateDeficit());
+
+      $('#daily-result').html(calculateCalories());
+    });
+
+    // $('#age-colored-slide').css('width',checkSliderLength());
+
+    $('.box').hover(function(){
+      var widthDiff = 0.5 * (parseInt($('.box').css('width'))-parseInt($('#tooltiptext1').css('width')));
+      $('.tooltiptext').css('left', widthDiff + "px");
+    })
+
 
 
 });
